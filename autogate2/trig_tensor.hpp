7a64bd201286316447403b0692fc8e41c02799ea
robparrishqc <43890147+robparrishqc@users.noreply.github.com>
2021-10-06 16:20:31 -0700

    Update trig_tensor.hpp

--- autogate2/trig_tensor.hpp
+++ autogate2/trig_tensor.hpp
@@ -21,7 +21,33 @@ const std::vector<size_t>& shape() const { return shape_; }
 std::vector<TrigPolynomial>& data() { return data_; }
 // TODO expose size to user
 // TODO vector of strides
+    
+// => Math Functions <= //
+    
+TrigTensor operator+() const;
+TrigTensor operator-() const;
+    
+TrigTensor& operator+=(const TrigTensor& other);
+TrigTensor& operator-=(const TrigTensor& other);
+friend TrigTensor operator+(const TrigTensor& a, const TrigTensor& b);
+friend TrigTensor operator-(const TrigTensor& a, const TrigTensor& b);
 
+TrigTensor& operator+=(const std::complex<double>& scal);
+TrigTensor& operator-=(const std::complex<double>& scal);
+TrigTensor& operator*=(const std::complex<double>& scal);
+TrigTensor& operator/=(const std::complex<double>& scal);
+friend TrigTensor operator+(const std::complex<double>& scalar, const TrigTensor& tensor);
+friend TrigTensor operator+(const TrigTensor& tensor, const std::complex<double>& scalar);
+friend TrigTensor operator-(const std::complex<double>& scalar, const TrigTensor& tensor);
+friend TrigTensor operator-(const TrigTensor& tensor, const std::complex<double>& scalar);
+friend TrigTensor operator*(const std::complex<double>& scalar, const TrigTensor& tensor);
+friend TrigTensor operator*(const TrigTensor& tensor, const std::complex<double>& scalar);
+friend TrigTensor operator/(const TrigTensor& tensor, const std::complex<double>& scalar);
+    
+TrigTensor conj() const;
+TrigTensor T() const;
+TrigTensor dagger() const;
+    
 private:
 
 std::vector<size_t> shape_;
